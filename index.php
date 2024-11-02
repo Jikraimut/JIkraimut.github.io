9c915cc6f13dc7c5bca171d4da1c56985f303a8d
JikraImut <clonezra@gmail.com>
2024-11-02 11:43:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" /> <script src="jquery.min.js"></script> <title>Jikra Imut</title> <?php date_default_timezone_set("Asia/Jakarta"); if(isset($_GET['jawaban'])){$fp = fopen('dekatutorial.txt', 'r'); echo '<link rel="stylesheet" href="style.css" /></head><body><div class="top">Jawaban Script   <br />Deka Tutorial</div><div class="content">';while(!feof($fp)){echo "".fgets($fp)."<br>";} echo '</div></body></html>';die;fclose($fp);} if(isset($_POST['submit'])){ $date = date("d-M-Y (H:i)");$status = $_POST["Status"];$pesan = $_POST["Pesan"]; $fp = fopen('dekatutorial.txt', 'a'); fwrite($fp,$date."\nStatus :\n<strong>".$status."\n</strong> Pesan :\n<strong>".$pesan."</strong>\n\n"); fclose($fp);index(); } function index(){ $link = "";if(isset($_GET["link"])){$link = $_GET["link"];}echo '<link rel="stylesheet" href="page2.css" /></head><body><script src="page2.js"></script><script>timeku("'.$link.'");</script></body></html>';die;} ?> <script>var musik = ""; var backgound = ""; var gambarStiker1 = ""; var gambarStiker2 = ""; var gambarStiker3 = "";</script> <script src="all.min.js"></script> <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" /> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> <title>Jikra Imut</title> <?php date_default_timezone_set("Asia/Jakarta"); if(isset($_GET['jawaban'])){$fp = fopen('dekatutorial.txt', 'r'); echo '<link rel="stylesheet" href="https://dekatutorial.github.io/18nembak/tampil.css" /></head><body><div class="top">Jawaban Script   <br />Deka Tutorial</div><div class="content">';while(!feof($fp)){echo "".fgets($fp)."<br>";} echo '</div></body></html>';die;fclose($fp);} if(isset($_POST['submit'])){ $date = date("d-M-Y (H:i)");$status = $_POST["Status"];$pesan = $_POST["Pesan"]; $fp = fopen('dekatutorial.txt', 'a'); fwrite($fp,$date."\nStatus :\n<strong>".$status."\n</strong> Pesan :\n<strong>".$pesan."</strong>\n\n"); fclose($fp);index(); } function index(){ $link = "";if(isset($_GET["link"])){$link = $_GET["link"];}echo '<link rel="stylesheet" href="https://dekatutorial.github.io/18nembak/page2.css" /></head><body><script src="https://dekatutorial.github.io/18nembak/page2.js"></script><script>timeku("'.$link.'");</script></body></html>';die;} ?> <script>var musik = ""; var backgound = ""; var gambarStiker1 = ""; var gambarStiker2 = ""; var gambarStiker3 = "";</script> <script src="https://dekatutorial.github.io/sweetalert2.all.min.js"></script> <link rel="stylesheet" href="https://dekatutorial.github.io/18nembak/style.css" />
   </head> <body> <script> popupku(); </script> <div class="preload"> <p>Loading dulu . . .</p> </div> <div class="open"> <div> <div class="lope" onclick="ilang()"><i class="love material-icons-sharp"> favorite </i></div> <h3>Pencet Love nya</h3> </div> </div> <div class="bg"></div> <div class="lopeBg"></div>
     <div class="top stiker"><img src="" /><img src="" /><img src="" /></div><div class="ucapan">
       <div id="slider">
@@ -28,7 +28,7 @@
     <script>
 
       // PENTING : jika ekstensi file berbeda silahkan ganti di bawah ini
-      musik = "imut.mp3";
+      musik = "FileMusik.mp3";
       background = "FileBackground.jpg";
       gambarStiker1 = "FileStiker1.gif";
       gambarStiker2 = "FileStiker2.gif";
@@ -48,6 +48,6 @@
         <button>Kirim Pesan</button>
       </div>
     </div>
-    <div class="container-pesan" style="display: none"> <div class="kirim-pesan"> <p>Kirim Pesan</p> <form method="post"> <input name="Status" hidden class="jawaban" type="text" value="" /> <textarea required name="Pesan" id="" cols="30" rows="10" placeholder="Tulis pesan..."></textarea> <button type="submit" name="submit">Kirim</button> </form> </div> </div> <script src="script.js"></script>
+    <div class="container-pesan" style="display: none"> <div class="kirim-pesan"> <p>Kirim Pesan</p> <form method="post"> <input name="Status" hidden class="jawaban" type="text" value="" /> <textarea required name="Pesan" id="" cols="30" rows="10" placeholder="Tulis pesan..."></textarea> <button type="submit" name="submit">Kirim</button> </form> </div> </div> <script src="https://dekatutorial.github.io/18nembak/script.js"></script>
   </body>
 </html>
